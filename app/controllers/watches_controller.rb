9376b20a988d7048c3ee5c84e9a958caf9bd287e
Alex Daish <alexdaish@Alexs-MacBook-Pro.local>
2015-04-30 13:03:08 +0100

    method for delete, create and new defined

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -7,19 +7,40 @@ class WatchesController < ApplicationController
   end
 
   def new
+  	@watch = Watch.new
   end
 
   def create
+  	@watch = Watch.new(watch_params)
+
+  	if @watch.save
+  		flash[:success]= "Thanks for uploading"
+  		redirect_to @watch
+  	else
+  		flash[:error] = "Please check form for error and try again"
+  		render :new
+  	end
   end
 
   def edit
+  	@watch = Watch.find(params[:id])
   end
 
   def update
+  	@watch = Watch.find(params[:id])
   end
 
   def destroy
+  	@watch = Watch.find(params[:id])
+  		
+	flash[:success] = "#{@watch.name} was removed successfully" if @watch.destroy
+	redirect_to root_path
   end
  
  end
 end
+
+	private
+	def watch_params
+		params.require(:watch).permit(:name, :description, :price_in_pence, :gender, :style, :brand, :pre_owned)
+	end
--- app/models/watch.rb
+++ app/models/watch.rb
@@ -1,2 +1,6 @@
 class Watch < ActiveRecord::Base
+	validates :name, presence: true, uniqueness:true
+	validates :description, presence: true
+	validates :brand, presence: true
+	validates :price_in_pence, presence: true, numericality: {greater_than_or_equal_to: 100}
 end
