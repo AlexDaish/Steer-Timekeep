550889a5e2cc3ca94e129b4e5591279c4be95c0b
Alex Daish <alexdaish@Alexs-MacBook-Pro.local>
2015-04-30 17:08:37 +0100

    login helper added

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,9 +3,15 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+	helper_method :current_user, :logged_in?
+
   def current_user
-  	if session[:user_id].present?
+  	if logged_in?
   		User.find(session[:user_id])
   	end
+  end
+
+  def logged_in?
+  	session[:user_id].present?
   end
 end
