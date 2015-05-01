0a0e2c7bc32c06982711990722b03f648a069472
Alex Daish <alexdaish@gmail.com>
2015-05-01 12:40:13 +0100

    create orders

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,33 @@
 class OrdersController < ApplicationController
+  before _action :require_user, except: [:show]
+
   def new
+  	@order = Order.new
+  end
+
+  def create
+  	# find the watch
+  	@watch = Watch.find(params[:watch_id])
+  	# create a new order with watch_id = @watch.id
+  	@order = @watch.order.new(order_params)
+  	# set user_id - current_user.id
+  	@order.user = current_user
+   	#charging code goes here
+  	if @order.save
+  		flash[:success] = "Order made"
+  		redirect_to @order
+  	else
+  		flash[:error] = 'Please check the form for errors and try again'
+  		render :new
+  	end
   end
 
   def show
+  	@order = Order.find(params[id])
+  	require_owner(@order) # make sure order.user_id == current_user.id
   end
+
+  private
+  def order_params
+  	params.require(:order).permit(:stripe_token)
 end
