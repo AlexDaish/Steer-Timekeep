c4754a25883a64ba17581350a35adc60036e77ba
Alex Daish <alexdaish@gmail.com>
2015-05-01 15:12:37 +0100

    Added stripe keys

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,4 +2,16 @@ class Order < ActiveRecord::Base
 	belongs_to :user
 	belongs_to :watch
 	validates :stripe_token, presence:true
+
+	def save_and_charge
+
+		Stripe.api_key = Rails.application.secrets.stripe_secret_key
+
+		Stripe::Charge.create(
+		  :amount => 400,
+		  :currency => "gbp",
+		  :source => "tok_15xeXJHFoAOwso1qp6lC2vDN", # obtained with Stripe.js
+		  :description => "Charge for test@example.com"
+		)
+	end
 end
